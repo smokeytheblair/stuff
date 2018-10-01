68d0fbd13fe41fcb625abf2e5bbc2749362ba102
Jeff Blair <jblair@Jeff-Blairs-MBP.lehi.saltstack.net>
2018-10-01 12:35:14 -0600

    fix missing case 'h'

--- big_switch.c
+++ big_switch.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 
-#define IS_a_TO_Z 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'i': case 'j': case 'k': \
+#define IS_a_TO_Z 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': \
                   case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': \
                   case 'v': case 'w': case 'x': case 'y': case 'z': case 'A': case 'B': case 'C': case 'D': case 'E': \
                   case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': \
