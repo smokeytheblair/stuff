e56c2892efc160b9245fe85b50d162d8e1eced86
Jeff Blair <jeffreyblair@gmail.com>
2018-10-30 15:27:01 -0600

    added type `long long`

--- sizeof_types.c
+++ sizeof_types.c
@@ -6,6 +6,7 @@ int main ()
   printf("pointer is %ld bytes\n", sizeof(void*));
   printf("float is %ld bytes\n", sizeof(float));
   printf("double is %ld bytes\n", sizeof(double));
+  printf("long long is %ld byte\n", sizeof(long long));
   printf("long is %ld bytes\n", sizeof(long));
   printf("int is %ld bytes\n", sizeof(int));
   printf("short is %ld bytes\n", sizeof(short));
