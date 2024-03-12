f31e7ced99e12a6f5d83bfbae438201cdc59c9ad
Eric Moncada <emoncada@vmware.com>
2024-03-12 17:11:56 -0600

    Working count primes for js up to 10000000000

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,89 +1,70 @@
-// compute primes up to max number using the Sieve algorithm
-// https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+class BigUint8Array {
+  constructor(size, fill, max) {
+    this.size = size;
+    this.fill = fill;
+    this.max = max || 4294967295;
 
+    this.arrays = [];
 
-function print_usage(name) {
-    console.log(`node ${name} <max_number>`);
+    this.init();
+  }
+
+  init() {
+    const numArrays = Math.ceil(this.size / this.max);
+    for (let i = 0; i < numArrays; i++) {
+      const size = i < numArrays - 1 ? this.max : this.size - this.max * i;
+      this.arrays.push(new Uint8Array(size).fill(this.fill));
+    }
+  }
+
+  set(index, value) {
+    const arraysIndex = ~~(index / this.max);
+    if (typeof this.arrays[arraysIndex] === "undefined") {
+      throw new Error(`No Uint8Array at index: ${arraysIndex}`);
+    }
+
+    const arrayIndex = index - arraysIndex * this.max;
+    if (typeof this.arrays[arraysIndex][arrayIndex] === "undefined") {
+      throw new Error(`Index "${arraysIndex}" is out of bounds`);
+    }
+
+    this.arrays[arraysIndex][arrayIndex] = value;
+  }
+
+  get(index) {
+    const arraysIndex = ~~(index / this.max);
+    if (typeof this.arrays[arraysIndex] !== "undefined") {
+      const arrayIndex = index - arraysIndex * this.max;
+      if (typeof this.arrays[arraysIndex][arrayIndex] !== "undefined") {
+        return this.arrays[arraysIndex][arrayIndex];
+      }
+    }
+    return undefined;
+  }
 }
 
-function* range(start, stop, step = 1) {
-    if (typeof stop === 'undefined') {
-        // one param defined
-        stop = start;
-        start = 0;
+if (typeof process.argv[2] === "undefined") {
+  console.log(`node ${process.argv[1]} <max_number>`);
+} else {
+  const max = parseInt(process.argv[2], 10);
+  if (!Number.isFinite(max) || max < 2)
+    throw new Error("<max_number> must be an integer greater than 2");
+
+  const startTime = performance.now();
+  const primes = new BigUint8Array(max + 1, 1);
+  for (let i = 2; i ** 2 <= max; i++) {
+    if (primes.get(i)) {
+      for (let j = i ** 2; j <= max; j += i) {
+        primes.set(j, 0);
+      }
     }
+  }
 
-    for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
-        yield i;
-    }
-}
+  let ans = 0;
+  for (let i = 2; i <= max; i++) {
+    if (primes.get(i)) ans++;
+  }
 
-function create_number_list(max) {
-    var numbers = new Uint8Array(parseInt(max/2));
-    var counter = 0;
-    for (let i of range(1, max, 2)) {
-        numbers[counter] = 1;
-        counter += 1;
-    }
-    
-    numbers[0] = 0;
-
-    return numbers;
-}
-
-function mark_multiples_of_prime(numbers, prime_index, max) {
-    // print('Checking prime:{}'.format(prime))
-
-    prime = (prime_index*2)-1;
-    array_length = numbers.length;
-
-    // the for loop will start at one multiple past the current prime
-    // it will walk the list of numbers by multiples of prime
-    // this logic removes the need for any if statements inside the loop
-    for (let i of range(prime_index+prime, array_length, prime)) {
-        numbers[i] = 0;
-    }
-}
-
-function compute_primes(max) {
-    var start_time = Date.now();
-    var numbers = create_number_list(max);
-
-    // console.log(`Computing primes from 0 to ${max}`);
-
-    half_length = parseInt(numbers.length / 2);
-
-    for (let index of range(2, half_length)) {
-        if (numbers[index] > 0) {
-            mark_multiples_of_prime(numbers, index, max);
-        }
-    }
-
-    var primes = 0;
-    for (let i of numbers) {
-        if (i > 0) {
-            primes += 1;
-        }
-    }
-
-    var finish_time = Date.now();
-    //console.log(primes);
-
-    // console.log(`Start: ${parseInt(start_time/1000)}, Finish: ${parseInt(finish_time/1000)}`);
-    var time = (parseInt((finish_time - start_time)));
-    console.log(`Compute time: ${time} milliseconds.`);
-    console.log(`Found ${primes} prime numbers less than ${max}`);
-}
-
-function main() {
-    if (process.argv.length > 2) {
-        compute_primes(parseInt(process.argv[2]));
-    } else {
-        print_usage(process.argv[1]);
-    }
-}
-
-
-if (require.main === module) {
-    main();
+  console.log(`Compute time: ${performance.now() - startTime} milliseconds.`);
+  console.log(`Found ${ans} prime numbers less than ${max}`);
 }
