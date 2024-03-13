adc3392a6da5227e0a81b6d8c2e577b529a93517
Eric Moncada <theskabeater@gmail.com>
2024-03-12 19:11:23 -0600

    Small optimizations and use native array if max is within limits

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,70 +1,51 @@
-class BigUint8Array {
-  constructor(size, fill, max) {
-    this.size = size;
-    this.fill = fill;
-    this.max = max || 4294967295;
-
-    this.arrays = [];
-
-    this.init();
+// solves limitations with array size in JS
+const bigArray = (size, fill, limit) => {
+  const l = size > limit ? -~(size / limit) : 1;
+  const api = {};
+  // just use a native Uint8Array if we're within the limit
+  if (l === 1) {
+    const array = new Uint8Array(size).fill(fill);
+    api.get = (index) => array[index];
+    api.set = (index, value) => (array[index] = value);
+  } else {
+    const arrays = Array(l);
+    for (let i = 0; i < l; i++) {
+      const k = i < l - 1 ? limit : size - limit * i;
+      arrays[i] = new Uint8Array(k).fill(fill);
+    }
+    api.get = (index) => {
+      const arraysIndex = ~~(index / limit);
+      const arrayIndex = index - arraysIndex * limit;
+      return arrays[arraysIndex][arrayIndex];
+    };
+    api.set = (index, value) => {
+      const arraysIndex = ~~(index / limit);
+      const arrayIndex = index - arraysIndex * limit;
+      arrays[arraysIndex][arrayIndex] = value;
+    };
   }
+  return api;
+};
 
-  init() {
-    const numArrays = Math.ceil(this.size / this.max);
-    for (let i = 0; i < numArrays; i++) {
-      const size = i < numArrays - 1 ? this.max : this.size - this.max * i;
-      this.arrays.push(new Uint8Array(size).fill(this.fill));
-    }
-  }
+// validate inputs
+const max = parseInt(process.argv[2], 10);
+if (!Number.isFinite(max) || max < 2) {
+  throw new Error("<max_number> must be an integer greater than 2");
+}
 
-  set(index, value) {
-    const arraysIndex = ~~(index / this.max);
-    if (typeof this.arrays[arraysIndex] === "undefined") {
-      throw new Error(`No Uint8Array at index: ${arraysIndex}`);
-    }
-
-    const arrayIndex = index - arraysIndex * this.max;
-    if (typeof this.arrays[arraysIndex][arrayIndex] === "undefined") {
-      throw new Error(`Index "${arraysIndex}" is out of bounds`);
-    }
-
-    this.arrays[arraysIndex][arrayIndex] = value;
-  }
-
-  get(index) {
-    const arraysIndex = ~~(index / this.max);
-    if (typeof this.arrays[arraysIndex] !== "undefined") {
-      const arrayIndex = index - arraysIndex * this.max;
-      if (typeof this.arrays[arraysIndex][arrayIndex] !== "undefined") {
-        return this.arrays[arraysIndex][arrayIndex];
-      }
-    }
-    return undefined;
+// fill sieve
+const startTime = performance.now();
+const primes = bigArray(max + 2, 1, 4294967295);
+for (let i = 2; i ** 2 < max; i++) {
+  if (primes.get(i)) {
+    // mark non-primes
+    for (let j = i ** 2; j < max; j += i) primes.set(j, 0);
   }
 }
 
-if (typeof process.argv[2] === "undefined") {
-  console.log(`node ${process.argv[1]} <max_number>`);
-} else {
-  const max = parseInt(process.argv[2], 10);
-  if (!Number.isFinite(max) || max < 2)
-    throw new Error("<max_number> must be an integer greater than 2");
+// count unmarked as primes
+let ans = 0;
+for (let i = 0; i < max; i++) if (primes.get(i)) ans++;
 
-  const startTime = performance.now();
-  const primes = new BigUint8Array(max + 1, 1);
-  for (let i = 2; i ** 2 <= max; i++) {
-    if (primes.get(i)) {
-      for (let j = i ** 2; j <= max; j += i) {
-        primes.set(j, 0);
-      }
-    }
-  }
-
-  let ans = 0;
-  for (let i = 2; i <= max; i++) {
-    if (primes.get(i)) ans++;
-  }
-
-  console.log(`Compute time: ${performance.now() - startTime} milliseconds.`);
-  console.log(`Found ${ans} prime numbers less than ${max}`);
-}
+console.log(`Compute time: ${performance.now() - startTime} milliseconds.`);
+console.log(`Found ${ans} prime numbers less than ${max}`);
